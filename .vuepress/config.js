332cb3e8c2569e30b777b2fd21e0b7a5d6426155
TopUnicorn2020 <63044100+TopUnicorn2020@users.noreply.github.com>
2020-04-02 17:21:05 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     port: "3000",
     dest: "docs",
     ga: "UA-85414008-1",
-    base: "/newBook/",
+    base: "/site/",
     markdown: {
         externalLinks: {
             target: '_blank', rel: 'noopener noreferrer'
